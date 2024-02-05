1dac11b9876fa12ccbd83fb604ad87ac5e223bcd
Mellblomman <115022366+Mellblomman@users.noreply.github.com>
2024-02-05 11:42:46 +0100

    Create querys.sql
    Added a query file with all the querys needed at the moment. Change bookings and edit courts.

---
+++ querys.sql
@@ -0,0 +1,21 @@
+UPDATE court
+SET price = new_price
+WHERE court_id = your_court_id;
+
+
+UPDATE court
+SET availability = CASE WHEN availability = 'True' THEN 'False' ELSE 'True' END
+WHERE court_id = your_court_id;
+
+
+INSERT INTO court (court_id, activity, datetime, price, availability)
+VALUES (your_court_id, 'New Activity', 'New DateTime', new_price, 'Available');
+
+
+DELETE FROM court
+WHERE court_id = your_court_id;
+
+
+UPDATE bookings
+SET datetime = new_datetime
+WHERE booking_id = your_booking_id;
