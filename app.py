1131e2d9335a7ffd233c4633f8add4386bb6e631
Enkas1 <lukas.engdahl@hotmail.com>
2024-02-06 20:16:10 +0100

    kopplat ihop frontend med backend. avbokningar

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, url_for
 import os
 import calendar
+import psycopg2
 from datetime import datetime
 app = Flask(__name__)
 
@@ -20,9 +21,9 @@ def render_activities():
 def render_cancellation():
     return render_template("cancellation.html")
 
-@app.route("/confirmationcancellation.html", methods=['POST'])
-def confirmationcancellation():
-    return render_template("confirmationcancellation.html", message="<span style='color: white;'>Tack för din avbokning, vi hoppas att vi syns inom snar framtid!</span>")
+#@app.route("/confirmationcancellation.html", methods=['POST'])
+#def confirmationcancellation():
+#    return render_template("confirmationcancellation.html", message="<span style='color: white;'>Tack för din avbokning, vi hoppas att vi syns inom snar framtid!</span>")
 
 
 @app.route("/badminton.html", methods=["GET"])
@@ -70,6 +71,66 @@ def render_padelbooking():
 @app.route("/padelbookingconfirmed.html", methods=["POST"])
 def render_padelbookingconfirmed():
     return render_template("padelbookingconfirmed.html")
+
+
+
+
+conn_details = {
+    "host": "localhost",
+    "database": "postgres",
+    "user": "postgres",
+    "password": "megaine11",
+    "port": '5432'
+}
+
+
+@app.route("/confirmationcancellation.html", methods=["POST"])
+def delete_booking():
+    booking_id = request.form.get("booking_id") # Sparar datan användaren skriver in på hemsidan i booking_id variabeln
+    if booking_id: # Om användaren matar in någon data.
+        if delete_booking_from_database(booking_id):
+            return render_template("confirmationcancellation.html", message="Avbokat")
+        else:
+            return render_template("confirmationcancellation.html", message="hittade inget a")
+            
+       
+def delete_booking_from_database(booking_id): # Funktion som kollar om booking_id finns i databasen och raderar
+    try: # Anslutning till databas
+        conn = psycopg2.connect(**conn_details) # **conn_details i Python betyder att du "expanderar" dictionaryn conn_details till nyckel-värde-par
+        cur = conn.cursor() #  Verktyg för att interagera med databaser från Python-kod.
+        cur.execute("DELETE FROM bookings WHERE booking_id = %s", (booking_id,)) # utför en operation men inget är permanent. conn.commit() gör det permanent.
+        conn.commit() # används för att "bekräfta" alla ändringar som gjorts under den aktuella transaktionen, DVS raderingen av booking_id i databasen
+        cur.close() # Stänger cursor eftersom vi inte behöver den mer, frigör resurser.
+        conn.close() # Stänger anslutningen till postgreSQL
+        return True 
+    except psycopg2.Error as e:
+        print("Error deleting booking:", e) # Vid anslutningsfel eller felaktig syntax i sql-fråga.
+        return False
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
