b08099839279ee1bcc9770c87284b5ae328cf6da
Enkas1 <lukas.engdahl@hotmail.com>
2024-02-20 16:45:34 +0100

    Deleted loginconfirmationcontact.html, Fixed confirmation on logincontact.html instead.

--- app.py
+++ app.py
@@ -116,9 +116,39 @@ def render_logincancellation():
 def render_loginboka():
     return render_template("loginboka.html")
 
-@app.route("/logincontact.html", methods=["GET"])
+@app.route("/logincontact.html", methods=["GET", "POST"])
 def render_logincontact():
-    return render_template("logincontact.html")
+    if request.method == "POST":
+        # Kontrollera om filen meddelande.txt finns, annars skapas den
+        if not os.path.isfile("meddelanden.txt"):
+            with open("meddelanden.txt", "w", encoding="utf-8"):
+                pass  # Skapar filen om den inte finns
+
+        # Regex-mönster för att validera e-postadress
+        email_pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+        # Regex-mönster för att validera telefonnummer (exakt 10 siffror)
+        phone_pattern = r"^\d{10}$"
+        # Regex-mönster för att validera meddelandet (minst 3 tecken)
+        message_pattern = r"^(?=\s*\S)(.{3,}(?:\s+\S+){0,299}\s*)$"
+
+        # Validera e-postadress
+        if not re.match(email_pattern, request.form["email"]):
+            return render_template("/logincontact.html", message="<span style='color: white;'>Felaktig e-postadress!</span>")
+
+        # Validera telefonnummer
+        if not re.match(phone_pattern, request.form["telefon"]):
+            return render_template("/logincontact.html", message="<span style='color: white;'>Felaktigt telefonnummer, fyll i 10 siffror!</span>")
+
+        # Validera meddelandet
+        if not re.match(message_pattern, request.form["message"]):
+            return render_template("/logincontact.html", message="<span style='color: white;'>Meddelandet måste vara minst 3 tecken långt!</span>")
+
+        # Om allt är korrekt, spara datan
+        with open("meddelanden.txt", "a", encoding="utf-8") as file:
+            file.write(f"{request.form['email']}, {request.form['telefon']}, {request.form['message']}\n")
+        return render_template("/logincontact.html", message="<span style='color: white;'>Tack för ditt mail, vi återkommer inom kort.</span>")
+    else:
+        return render_template("logincontact.html")
 
 @app.route("/loginindex.html", methods=["GET"])
 def render_loginindex():
@@ -135,39 +165,7 @@ def delete_login_booking():
     else:
         return render_template("loginconfirmationcancellation.html", message="Inget bokningsid angivet")
 
-@app.route("/loginconfirmationcontact.html", methods=["POST", "GET"])
-def render_login_confirmationcontact():
-    if request.method == "POST":
-        # Kontrollera om filen meddelande.txt finns, annars skapas den
-        if not os.path.isfile("meddelanden.txt"):
-            with open("meddelanden.txt", "w", encoding="utf-8"):
-                pass  # Skapar filen om den inte finns
 
-        # Regex-mönster för att validera e-postadress
-        email_pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
-        # Regex-mönster för att validera telefonnummer (exakt 10 siffror)
-        phone_pattern = r"^\d{10}$"
-        # Regex-mönster för att validera meddelandet (minst 3 tecken)
-        message_pattern = r"^(?=\s*\S)(.{3,}(?:\s+\S+){0,299}\s*)$"
-
-        # Validera e-postadress
-        if not re.match(email_pattern, request.form["email"]):
-            return render_template("/loginconfirmationcontact.html", message="<span style='color: white;'>Felaktig e-postadress!</span>")
-
-        # Validera telefonnummer
-        if not re.match(phone_pattern, request.form["telefon"]):
-            return render_template("/loginconfirmationcontact.html", message="<span style='color: white;'>Felaktigt telefonnummer, fyll i 10 siffror!</span>")
-
-        # Validera meddelandet
-        if not re.match(message_pattern, request.form["message"]):
-            return render_template("/loginconfirmationcontact.html", message="<span style='color: white;'>Meddelandet måste vara minst 3 tecken långt!</span>")
-
-        # Om allt är korrekt, spara datan
-        with open("meddelanden.txt", "a", encoding="utf-8") as file:
-            file.write(f"{request.form['email']}, {request.form['telefon']}, {request.form['message']}\n")
-        return render_template("/loginconfirmationcontact.html", message="<span style='color: white;'>Tack för ditt mail, vi återkommer inom kort.</span>")
-    else:
-        return "Metoden är inte tillåten"
     
 @app.route("/loginbookingconfirmed.html", methods=["POST", "GET"])
 def de_login_booking():
